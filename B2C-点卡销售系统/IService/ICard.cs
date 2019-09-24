7805ff762468868e430e580dacf4f8f41454e012
5645q <45084201+5645q@users.noreply.github.com>
2019-09-24 16:18:25 +0800

    11

---
+++ B2C-点卡销售系统/IService/ICard.cs
@@ -0,0 +1,27 @@
+﻿using B2C.DTO;
+using B2C.IService;
+using Service.Entitie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2C.IDAL
+{
+    public interface ICard: IServiceSupport
+    {
+        CardDTO SelectCardByID(long t_cardId);
+
+        CardDTO[] GetAllCard();
+
+        CardDTO[] GetAllCardByCardTypeId(long t_cardTypeId);
+
+        CardDTO[] GetAllCardByCardState(long t_cardState);
+
+
+        long InsertCard(CardDTO t_Card);
+
+        long UpdateCard(CardDTO t_Card);
+
+        void DeleteCard(long t_cardId);
+    }
+}
---
+++ B2C-点卡销售系统/IService/ICardState.cs
@@ -0,0 +1,22 @@
+﻿using B2C.DTO;
+using B2C.IService;
+using Service.Entitie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2C.IDAL
+{
+    public interface ICardState: IServiceSupport
+    {
+        CardStateDTO SelectCardStateByID(long t_cardStateId);
+
+        CardStateDTO[] GetAllCardState();
+
+        long InsertCardState(CardStateDTO t_CardState);
+
+        long UpdateCardState(CardStateDTO t_CardState);
+
+        void DeleteCardState(long t_cardStateId);
+    }
+}
