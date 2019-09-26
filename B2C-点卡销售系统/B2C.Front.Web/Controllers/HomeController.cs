2f61714344a25228b5f481c091dfb8d6384106ca
lyc <1359001996@qq.com>
2019-09-26 11:08:50 +0800

    HomeController

---
+++ B2C-点卡销售系统/B2C.Front.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace B2C.Front.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult SelectPwd()
+        {
+            return View();
+        }
+    }
+}
